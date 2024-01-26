b3b503f5dd2aacb20f2b3b7bb5ed38d3f8aab837
amhsm6 <amhsm6@gmail.com>
2024-01-26 11:44:53 +0200

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -11,9 +11,15 @@ import System.FilePath
 import System.Directory
 import qualified TD.GeneralResult as GR
 import qualified TD.Data.Update as U
-import qualified TD.Data.Chats as CS
-import TD.Query.SetLogVerbosityLevel
-import TD.Query.SearchChatsOnServer
+import qualified TD.Data.User as User
+import qualified TD.Data.Chat as Chat
+import qualified TD.Data.InputMessageContent as IMC
+import qualified TD.Data.FormattedText as FT
+import qualified TD.Query.SetLogVerbosityLevel as SLVL
+import qualified TD.Query.GetMe as GM
+import qualified TD.Query.CreatePrivateChat as CPC
+import qualified TD.Query.GetChatHistory as GCH
+import qualified TD.Query.SendMessage as SM
 
 import Action
 import Init
@@ -50,18 +56,37 @@ main :: IO ()
 main = do
     initial <- scan
     fresh $ do
-        send $ SetLogVerbosityLevel $ Just 2
+        send $ SLVL.SetLogVerbosityLevel $ Just 2
         auth
 
-        send $ SearchChatsOnServer (Just $ T.pack "Saved Messages") $ Just 1
-        let getChat = do
-                liftIO $ putStrLn "Trying.."
+        send GM.GetMe
+        let getMe = do
                 x <- recv
-                liftIO $ print $ fst <$> x
                 case x of
-                    Just (GR.Chats (CS.Chats (Just 1) (Just [id])), _) -> pure id
-                    Just (GR.Chats _, _) -> end
+                    Just (GR.User user, _) -> pure user
+                    _ -> getMe
+        me <- getMe >>= unwrap . User._id
+
+        send $ CPC.defaultCreatePrivateChat { CPC.user_id = Just me }
+        let getChat = do
+                x <- recv
+                case x of
+                    Just (GR.Chat chat, _) -> pure chat
                     _ -> getChat
-        id <- getChat
+        id <- getChat >>= unwrap . Chat._id
 
         liftIO $ print id
+
+        send $ GCH.defaultGetChatHistory { GCH.chat_id = Just id, GCH.from_message_id = Just 0, GCH.offset = Just (-30), GCH.limit = Just 60 }
+        let getHistory = do
+                x <- recv
+                case x of
+                    Just (GR.Messages msgs, _) -> pure msgs
+                    _ -> getHistory
+        msgs <- getHistory
+
+        liftIO $ print msgs
+
+        {-send $ SM.defaultSendMessage { SM.chat_id = Just id
+                                  , SM.input_message_content = Just $ IMC.InputMessageText (Just $ FT.defaultFormattedText { FT.text = Just $ T.pack "111" }) Nothing Nothing
+                                  }-}
